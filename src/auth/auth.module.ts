d03d4cdb67cd779c6ee7432731b0b79d9889f40f
ชื่อSupakon2543 <supakon.panjaiyen@stream.co.th>
2025-06-09 16:08:43 +0700

    auth #4

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,20 @@
 import { Module } from '@nestjs/common';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
-import { UsersService } from 'src/users/users.service';
-import { UserRoleService } from 'src/user_role/user_role.service';
-import { PrismaService } from 'src/prisma/prisma.service';
 const secret = process.env.JWTSECRET || '515b400cc9024b3a97fc25aceebb71e3';
-console.log('Secret Value:', secret);
-console.log('JWT Secret From .ENV:', process.env.JWTSECRET); // Log the secret for debugging purposes
+
 @Module({
   imports: [
     PassportModule,
     JwtModule.register({
-      secret: '515b400cc9024b3a97fc25aceebb71e3', // Use env variable in production!
+      secret: secret, // Use env variable in production!
       signOptions: { expiresIn: '3h' },
     }),
   ],
-  providers: [AuthService, JwtStrategy, UsersService, UserRoleService, PrismaService, JwtService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
