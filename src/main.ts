78ee80625e07c202985b847c0fd5714ca9137e0e
ชื่อSupakon2543 <supakon.panjaiyen@stream.co.th>
2025-06-25 14:02:41 +0700

    add/api in endpoint

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix('api/v1');
 
   await app.listen(process.env.PORT ?? 3000);
 }
