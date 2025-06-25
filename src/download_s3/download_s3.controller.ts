9693976227091aa6a470fd975ac88b1478e29913
ชื่อSupakon2543 <supakon.panjaiyen@stream.co.th>
2025-06-25 11:19:48 +0700

    add auth to s3

--- src/download_s3/download_s3.controller.ts
+++ src/download_s3/download_s3.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
 import { DownloadS3Service } from './download_s3.service';
 import { CreateDowloadS3Dto } from './dto/create-dowload_s3.dto';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('download-s3')
 export class DownloadS3Controller {
   constructor(private readonly downloadS3Service: DownloadS3Service) {}
